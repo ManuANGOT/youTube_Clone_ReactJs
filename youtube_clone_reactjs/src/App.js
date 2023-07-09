6b399996cff83378d8b25247d5a58792d34a1701
Manu <manu.angot@hotmail.fr>
2023-07-09 14:16:27 +0200

    Navbar created Feeding in course

--- youtube_clone_reactjs/src/App.js
+++ youtube_clone_reactjs/src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@emui:material';
+
 import React from 'react';
 
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
 const App = () =>(
     <BrowserRouter>
         <Box sx = {{ backgroundColour: '#000'}}>
-        Navbar
+        <Navbar/>
         <Routes>
-            <Route path = "/" exact element = {<Feed/>} />
-            <Route path = "/video/:id"  />
+            <Route path = "/" exact element = {<Feed/>}/>
+            <Route path = "/video/:id" element = {<VideoDetail/>}/>
+            <Route path = "/channel/:id" element = {<ChannelDetail/>}/>
+            <Route path = "/search/searchTerm" element = {<SearchFeed/>}/>
         </Routes>
 
         </Box>
---
+++ youtube_clone_reactjs/src/components/Feed.jsx
@@ -0,0 +1,22 @@
+import {useState, useEffect} from 'react';
+import { Box, Stack, Typography } from '@muy/material';
+
+
+const Feed = () => {
+    
+        <Stack sx={{ flexDirection: { sx: 
+        "column", md:"row" }}}>
+            <Box sx={{ height:{ sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2} }}>
+                SideBar
+                <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color:'#fff'}}>
+                    Copyright 2023 -Clone- Simplon Rx
+                </Typography>
+
+            </Box>
+            
+          
+        </Stack>
+    
+}
+
+export default Feed;
